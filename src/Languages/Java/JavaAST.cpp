16a22fa19d1d1a6e4c2fe1dc7abdb2fc50f5d0a9
notansooon <ansonjiang043@gmail.com>
2025-03-03 15:57:41 -0500

    JavaAST Continued

--- src/Languages/Java/JavaAST.cpp
+++ src/Languages/Java/JavaAST.cpp
@@ -1,39 +1,54 @@
-#include "ASTProcessor.h";
+#include "ASTProcessor.h"
 #include <sstream>
+#include <memory>
+#include <string>
 
-
+using namespace std;
 
 class JavaAST : public ASTProcessor {
 public:
+    shared_ptr<ASTNode> processNode(const string &compileOutput) override {
+        istringstream ss(compileOutput);
+        string line;
+
+        shared_ptr<ASTNode> root = make_shared<ASTNode>();
+
+        //return nullPtr if AST is invalid (for error handler)
+        if (compileOutput.find("error") != string::npos) {
+           return nullptr;
+        }
 
 
-//
-shared_ptr<ASTNode> processNode(const string &compileOutput) override {
+        while (getline(ss, line)) {
+            if (line.find("class") != string::npos) {
+                // Process class declaration here
+                root->children.push_back(make_shared<ASTNode>("class", line, 1));
+            }
+            else if (line.find("Method") != string::npos) {
+                      //implement later
+            }
+            else if (line.find("Variable") != string::npos) {
+                      //implement later
+            }
+        }
 
-           istringstream ss(compileOutput);
-           string line;
+        return root; // Ensure function returns a value
+    }
 
-           shared_ptr<ASTNode> root = make_shared<ASTNode>();
+    void analyzeAST(const shared_ptr<ASTNode> &root) override {
+        // Analyze AST implementation here
+    }
 
-           while (getline(ss, line)) {
+           //Helper method for extracting line number
+    private:
+           string extractName(const string &line){
+                      int position = line.find(" ");
 
-           if(line.find("class") != string::npos) {
+                      if (position != string::npos) {
+                                 return line.substr(position + 1);
+                      }
 
-                      children
 
            }
-
-           }
-
-}
-
-void analyzeAST(const shared_ptr<ASTNode> &root) override {
-
-}
-
-
-
-
-
-
+           
 };
