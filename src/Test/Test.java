d2f0b9cbd9b4ec1d7a68c360f3ae5c4695a0b360
notansooon <ansonjiang043@gmail.com>
2025-03-08 22:12:37 -0500

    Test

--- src/Test/Test.java
+++ src/Test/Test.java
@@ -2,6 +2,7 @@ public class Test {
            public static void main(String[] args) {
                int x = 10;
                int y = 20;
+               int z = 0;
                int sum = x + y;
                System.out.println("Sum: " + sum);
            }
